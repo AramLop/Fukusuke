0742c67f370b1c58944ca881c9fea481e5c37bf2
Felipe <f.mellasaavedra201@uandresbello.edu>
2024-10-28 20:02:26 -0300

    Backend

--- sushi/src/App.js
+++ sushi/src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IniciarSesion from './components/IniciarSesion';
 import Pago from './components/Pago';
 import CajaVirtual from './components/CajaVirtual';
+import express from "express"
 function App () {
+  const app=express()
+  app.listen(4000)
+  console.log("Servidor en el puerto 4000")
   return (
     <Router>
       <Routes>
