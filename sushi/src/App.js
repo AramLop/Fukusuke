4187d69a604c0bd6e779a391f0e0709f29ad8a7f
Felipe <f.mellasaavedra201@uandresbello.edu>
2024-11-04 13:11:38 -0300

    Arreglo

--- sushi/src/App.js
+++ sushi/src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import IniciarSesion from './components/IniciarSesion';
-import Pago from './components/Pago';
-import CajaVirtual from './components/CajaVirtual';
-import express from "express"
-function App () {
-  const app=express()
-  app.listen(4000)
-  console.log("Servidor en el puerto 4000")
+import IniciarSesion from './components/IniciarSesion.jsx';
+import Pago from './components/Pago.jsx';
+import CajaVirtual from './components/CajaVirtual.jsx';
+
+function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<IniciarSesion />} />
         <Route path="/Pago" element={<Pago />} />
         <Route path="/CajaVirtual" element={<CajaVirtual />} />
-
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
